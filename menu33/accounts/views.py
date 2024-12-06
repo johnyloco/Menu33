71139d2a32b3b667701879030af624eca178f800
johnyloco <alexmail095@gmail.com>
2024-12-06 20:10:05 +0100

    againtry

--- menu33/accounts/views.py
+++ menu33/accounts/views.py
@@ -17,7 +17,7 @@ class AppUserRegisterView(CreateView):
     model = UserModel
     form_class = AppUserCreationForm
     template_name = 'accounts/register-page.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('home-page')
 
     def form_valid(self, form):
         response = super().form_valid(form)
