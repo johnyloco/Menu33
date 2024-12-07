5a283ab1cad310b29d7604fc683221111995f9ea
johnyloco <alexmail095@gmail.com>
2024-12-07 17:28:23 +0100

    change setting allowed host

--- menu33/settings.py
+++ menu33/settings.py
@@ -33,7 +33,11 @@ DEBUG = os.getenv('DEBUG', config('DEBUG')) == "True"
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', config('ALLOWED_HOSTS')).split(', ')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', config('CSRF_TRUSTED_ORIGINS', [])).split(', ')
+CSRF_TRUSTED_ORIGINS = [
+    'https://127.0.0.1',
+    'https://localhost',
+    'https://menus-g0h7hgb2hxgdehft.switzerlandnorth-01.azurewebsites.net',
+]
 
 # Application definition
 MY_APPS = [
